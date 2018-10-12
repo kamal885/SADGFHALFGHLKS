8430a09b4f134846d13b83d127ad403d3307cdcb
kamal885 <43897372+kamal885@users.noreply.github.com>
2018-10-12 18:59:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,4 +29,117 @@ client.on('ready', () => {
 
 
 
-client.login(process.env.BOT_TOKEN);
+
+
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "-";
+
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('message', message => {
+var prefix = "-";
+
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + '1bc') {
+    if (!args[1]) {
+message.channel.send("**bc <message>**");
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField('» السيرفر :', `${message.guild.name}`)
+            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
+
+
+
+
+
+ client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 1 **")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
+
+ 
+
+client.on("message", message => {
+ if (message.content === "-help") {
+	  message.channel.send('**تم ارسالك في الخاص** :mailbox_with_mail: ');
+  const embed = new Discord.RichEmbed() 
+      .setColor("#000000")
+      .setDescription(`
+	       اوامر البوت
+❖-bc ⇏ لارسال الي الونلاين بدون ازهر اسمك او اسم السرفر
+❖-1bc ⇏ لارسال الي الكل باسمك واسم السرفر
+❖ لو تب الربط ⇏ رابط
+❖-2bc ⇏ سمك واسم السرفر
+  by❖Darmex      ❖Sky Shop
+
+ `)
+   message.author.sendEmbed(embed)
+    
+   }
+   }); 
+
+   
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(` Bot- Script By : DaHoM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DaHoM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` No Ra7ma `,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+client.login("process.env.BOT_TOKEN");
